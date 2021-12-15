4d1e691dc1b3d250b03a1f7321b120f9e94fa283
suwan <jsuwan961205@gmail.com>
2021-12-15 13:27:03 +0900

    edit instance resource

--- main.tf
+++ main.tf
@@ -24,10 +24,18 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "terraform-example"
-    }
+resource "aws_instance" "test" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
+# resource "aws_instance" "example" {
+#     ami = "ami-0c55b159cbfafe1f0"
+#     instance_type = "t2.micro"
+#     tags = {
+#         Name = "terraform-example"
+#     }
+# }
