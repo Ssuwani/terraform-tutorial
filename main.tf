f6d7a7019ffbf8a1d3fb0a33bb44e3d2299d2763
suwan <jsuwan961205@gmail.com>
2021-12-15 13:18:26 +0900

    edit main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
   cloud {
     organization = "suwan"
 
@@ -6,4 +15,16 @@ terraform {
       name = "gh-actions"
     }
   }
+}
+
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "terraform-example"
+    }
 }
