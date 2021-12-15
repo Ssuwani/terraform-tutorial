15e9b50edbbcbb675e2351db04b988bff67abe91
suwan <jsuwan961205@gmail.com>
2021-12-15 18:51:59 +0900

    close my instance

--- main.tf
+++ main.tf
@@ -21,28 +21,34 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "test" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+// resource "aws_instance" "test" {
+//   ami                    = "ami-0c55b159cbfafe1f0"
+//   instance_type          = "t2.micro"
+//   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  tags = {
-    Name = "HelloWorld"
-  }
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
+//   tags = {
+//     Name = "HelloWorld"
+//   }
+//   user_data = <<-EOF
+//               #!/bin/bash
+//               echo "Hello, World" > index.html
+//               nohup busybox httpd -f -p ${var.server_port} &
+//               EOF
+// }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+// resource "aws_security_group" "instance" {
+//   name = "terraform-example-instance"
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+//   ingress {
+//     from_port   = var.server_port
+//     to_port     = var.server_port
+//     protocol    = "tcp"
+//     cidr_blocks = ["0.0.0.0/0"]
+//   }
+// }
+
+// variable "server_port"{
+//   description = "The port the server will use for HTTP requests"
+//   type = number
+//   default = 8080
+// }
